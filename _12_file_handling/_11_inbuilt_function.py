6de0330f911b077b4eb8d6d0c2b971d51651740d
Sridhar Raj P <134199490+CredoSridhar97@users.noreply.github.com>
2023-06-01 15:10:36 +0530

    Add files via upload

---
+++ _12_file_handling/_11_inbuilt_function.py
@@ -0,0 +1,32 @@
+file = open("code.txt","r")
+
+
+# print(file.read(4))
+
+# print(file.read(5))
+
+# print(file.read())
+
+# file.write("Bye all")
+
+# lst = ["Hello\n","Bye\n","Good Night\n"]
+# file.writelines(lst)
+
+# data = file.read() # reads the whole thing
+# print(data)
+
+# data = file.read(5) # reads the 5 characters
+# print(data)
+
+# data = file.readline() # only reads the first line
+# print(data)
+
+# data = file.readlines() # returns list of lines
+# print(data)
+# print(type(data))
+
+# for i in data:
+#     print(i)
+
+
+
